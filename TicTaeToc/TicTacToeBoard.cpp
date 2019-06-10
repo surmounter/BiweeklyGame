451e7fa309f74c0a4315cad446c35f460b1d4ad3
HongGyu Kim <surmounter3283@gmail.com>
2019-06-10 15:46:21 +0900

    feat : Implement TicTacToeBoard class

--- TicTaeToc/TicTacToeBoard.cpp
+++ TicTaeToc/TicTacToeBoard.cpp
@@ -1,6 +1,19 @@
 #include "TicTacToeBoard.h"
+#include <string.h>
 
-void TicTacToeBoard::InitializeBoard()
+TicTacToeBoard::TicTacToeBoard()
 {
-	
 }
+
+TicTacToeBoard::~TicTacToeBoard()
+{
+}
+
+const bool TicTacToeBoard::MarkBoard(const HorseType::value horseType, const BoardCoord & boardCoord)
+{
+	if (IsEmpty(boardCoord) == false || IsOffBoard(boardCoord) == false)
+		return false;
+	_board[boardCoord.y][boardCoord.x] = horseType;
+	return true;
+}
+
--- TicTaeToc/TicTacToeBoard.h
+++ TicTaeToc/TicTacToeBoard.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <utility>
+
+struct BoardCoord 
+{
+	int x, y;
+};
+
 struct HorseType
 {
-	enum {MARK_O = 0, MARK_X, NUM_OF_HORSE_TYPE};
+	enum value {EMPTY = 0, MARK_O, MARK_X, NUM_OF_HORSE_TYPE};
 };
 
 class TicTacToeBoard final
@@ -11,7 +18,24 @@ public:
 	TicTacToeBoard();
 	~TicTacToeBoard();
 
-	void InitializeBoard();
+	void ClearBoard()
+	{ 
+		memset(_board, HorseType::EMPTY, sizeof(_board)); 
+	}
+	const bool MarkBoard(const HorseType::value horseType, const BoardCoord& boardCoord);
+
 private:
-	int _board[3][3];
+	const bool IsEmpty(const BoardCoord& boardCoord) const
+	{
+		return _board[boardCoord.y][boardCoord.x] == HorseType::EMPTY; 
+	};
+
+	const bool IsOffBoard(const BoardCoord& boardCoord) const 
+	{
+		return (boardCoord.x < 0) || (boardCoord.x >= colCnt) || (boardCoord.y < 0) || (boardCoord.y >= colCnt);
+	};
+private:
+	const int rowCnt = 3;
+	const int colCnt = 3;
+	int _board[3][3]; // TODO : 상수를 배열 길이로 넣고 싶은데? 되는거 아닌가?
 };
