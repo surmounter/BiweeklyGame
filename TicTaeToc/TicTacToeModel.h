f6b959b145070621598a0e4d60bdac8c70dc5b9a
HongGyu Kim <surmounter3283@gmail.com>
2019-06-10 19:07:12 +0900

    feat : Add TicTacToeBoard and TicTacToeMissionMessageSet member variable

--- TicTaeToc/TicTacToeModel.h
+++ TicTaeToc/TicTacToeModel.h
@@ -1,8 +1,15 @@
 #pragma once
+#include <memory>
+class TicTacToeBoard;
+class TicTacToeMissionMessageSet;
 
 class TicTacToeModel final
 {
 public:
 	TicTacToeModel();
 	~TicTacToeModel();
+
+private:
+	std::unique_ptr<TicTacToeBoard> _board;
+	std::unique_ptr<TicTacToeMissionMessageSet> _missionMessageSet;
 };
