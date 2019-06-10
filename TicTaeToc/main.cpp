2a432b1f516b2c9f8a371dafb02124141b634ef7
HongGyu Kim <surmounter3283@gmail.com>
2019-06-10 13:30:53 +0900

    feat : Set up SFML library in project

---
+++ TicTaeToc/main.cpp
@@ -0,0 +1,33 @@
+// MyBiweeklyGame.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+// 이벤트 관련 페이지 : https://stackoverflow.com/questions/768351/complete-example-using-boostsignals-for-c-eventing
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+int main()
+{
+	std::cout << MessageBoxA(NULL, "text", "Title!!", MB_OKCANCEL | MB_ICONEXCLAMATION);
+	sf::Texture background;
+	sf::Sprite backgroundImage;
+	if (background.loadFromFile("board.png") == false)
+		std::cout << "can't find background image" << std::endl;
+	backgroundImage.setTexture(background);
+
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(800, 600), "TicTacToe", sf::Style::Close);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(backgroundImage);
+		window.display();
+	}
+	return 0;
+}
