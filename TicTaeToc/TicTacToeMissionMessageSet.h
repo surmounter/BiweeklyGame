af6b3b1d0027dc94921d7b342e21d8cb5a0fd857
HongGyu Kim <surmounter3283@gmail.com>
2019-06-10 19:03:38 +0900

    Cleanup : cleanup allign

--- TicTaeToc/TicTacToeMissionMessageSet.h
+++ TicTaeToc/TicTacToeMissionMessageSet.h
@@ -15,6 +15,7 @@ private:
 	void InitializeMissionMessages(const std::string& directory);	
 	void SetMissionMessagesFromFile(const std::string& directory);
 	void ShuffleMissionMessages();
+
 private:
 	const std::string missionMessageFileDir = "Resource/missionMessageFile.txt";
 	std::vector<std::string> _missionMessages;
