707d65a757fd1ed2e153af5d691954a71dc4c56a
HongGyu Kim <surmounter3283@gmail.com>
2019-06-10 18:41:40 +0900

    feat : Add InitializeMissionMessage Function

--- TicTaeToc/TicTacToeMissionMessageSet.cpp
+++ TicTaeToc/TicTacToeMissionMessageSet.cpp
@@ -1,4 +1,5 @@
 #include "TicTacToeMissionMessageSet.h"
+#include <fstream>
 
 TicTacToeMissionMessageSet::TicTacToeMissionMessageSet()
 {
@@ -6,4 +7,17 @@ TicTacToeMissionMessageSet::TicTacToeMissionMessageSet()
 
 TicTacToeMissionMessageSet::~TicTacToeMissionMessageSet()
 {
+}
+
+void TicTacToeMissionMessageSet::InitializeMissionMessages(const std::string directory)
+{
+	_missionMessages.clear();
+	std::ifstream missionMessageFile(directory.data());
+	if (missionMessageFile.is_open())
+	{
+		std::string line;
+		while (std::getline(missionMessageFile, line))
+			_missionMessages.push_back(line);
+		missionMessageFile.close();
+	}
 }
--- TicTaeToc/TicTacToeMissionMessageSet.h
+++ TicTaeToc/TicTacToeMissionMessageSet.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <string>
+#include <vector>
 
 class TicTacToeMissionMessageSet final
 {
 public:
 	TicTacToeMissionMessageSet();
 	~TicTacToeMissionMessageSet();
+
+private:
+	void InitializeMissionMessages(const std::string directory);
+private:
+	const std::string missionMessageFileDir = "Resources/missionMessageFile.txt";
+	std::vector<std::string> _missionMessages;
 };
