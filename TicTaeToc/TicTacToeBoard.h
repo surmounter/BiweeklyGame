741fdb9ac5039d1e538022781db7ae7c749fb108
HongGyu Kim <surmounter3283@gmail.com>
2019-06-11 14:45:41 +0900

    Cleanup : cleanup allign in TicTacToeBoard class

--- TicTaeToc/TicTacToeBoard.h
+++ TicTaeToc/TicTacToeBoard.h
@@ -16,24 +16,15 @@ public:
 	TicTacToeBoard();
 	~TicTacToeBoard();
 
-	void ClearBoard()
-	{ 
-		memset(_board, HorseType::EMPTY, sizeof(_board)); 
-	}
+	void ClearBoard() { memset(_board, HorseType::EMPTY, sizeof(_board)); }
 	const bool MarkBoard(const HorseType::value horseType, const BoardSlot& boardSlot);
 	const BoardSlot ConvertMouseCoordToBoardCoord(const WindowSize& windowSize, const Point& mouseCoord) const;
 	void ShowBoard() const;
 
 private:
-	const bool IsEmpty(const BoardSlot& boardSlot) const
-	{
-		return _board[boardSlot.y][boardSlot.x] == HorseType::EMPTY; 
-	};
+	const bool IsEmpty(const BoardSlot& boardSlot) const { return _board[boardSlot.y][boardSlot.x] == HorseType::EMPTY; };
+	const bool IsOffBoard(const BoardSlot& boardSlot) const { return (boardSlot.x < 0) || (boardSlot.x >= colCnt) || (boardSlot.y < 0) || (boardSlot.y >= colCnt); };
 
-	const bool IsOffBoard(const BoardSlot& boardSlot) const 
-	{
-		return (boardSlot.x < 0) || (boardSlot.x >= colCnt) || (boardSlot.y < 0) || (boardSlot.y >= colCnt);
-	};
 private:
 	const int rowCnt = 3;
 	const int colCnt = 3;
