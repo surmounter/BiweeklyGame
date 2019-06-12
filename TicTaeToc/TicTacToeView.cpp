4758aa1c0cddbd530c76f2494a0086c6420136fc
HongGyu Kim <surmounter3283@gmail.com>
2019-06-12 14:15:41 +0900

    feat : Add MapSpriteWithTexture method

--- TicTaeToc/TicTacToeView.cpp
+++ TicTaeToc/TicTacToeView.cpp
@@ -1,10 +1,23 @@
 #include "TicTacToeView.h"
+#include <iostream>
 
 TicTacToeView::TicTacToeView()
 {
-	
+	MapSpriteWithTexture(_boardSprite, _boardTexture, _boardTextureDir);
+	MapSpriteWithTexture(_markOSprite, _markOTexture, _markOTextureDir);
+	MapSpriteWithTexture(_markXSprite, _markXTexture, _markXTextureDir);
 }
 
 TicTacToeView::~TicTacToeView()
 {
+}
+
+void TicTacToeView::MapSpriteWithTexture(sf::Sprite & sprite, sf::Texture & texture, const std::string &imageDir)
+{
+	if (texture.loadFromFile(imageDir) == false)
+	{
+		std::cout << "cannot load texture in " << imageDir << std::endl;
+		return;
+	}
+	sprite.setTexture(texture);
 }
--- TicTaeToc/TicTacToeView.h
+++ TicTaeToc/TicTacToeView.h
@@ -8,12 +8,15 @@ public:
 	~TicTacToeView();
 
 private:
-
+	void MapSpriteWithTexture(sf::Sprite &sprite, sf::Texture &texture, const std::string &imageDir);
 private:
+	const std::string _boardTextureDir = "Resource/board.png";
 	sf::Texture _boardTexture;
 	sf::Sprite _boardSprite;
+	const std::string _markOTextureDir = "Resource/markO.png";
 	sf::Texture _markOTexture;
 	sf::Sprite _markOSprite;
+	const std::string _markXTextureDir = "Resource/markX.png";
 	sf::Texture _markXTexture;
 	sf::Sprite _markXSprite;
 };
